5fff28aae565050391c9b453a0e2b0e1c5411b44
Trevor Delamorandiere <tdelam@gmail.com>
2017-10-24 23:25:31 -0400

    transform menu items, loading from mongo and checking header and cutting

--- perform.py
+++ perform.py
@@ -4,6 +4,7 @@ import petl as etl
 import MySQLdb
 import sys
 import itertools
+import pymongo
 
 from petl.io.db import DbView
 from pymongo import MongoClient
@@ -19,29 +20,30 @@ def extract(table_name, show_tables=False):
     """
     Grab all data from source(s).
     """
-    db = MySQLdb.connect(host="localhost",
+    source_db = MySQLdb.connect(host="localhost",
                      user="root",
                      passwd="c0l3m4N",
                      db="mmjmenu_development")
-    #table = etl.fromdb(db, 'SELECT * FROM menu_items')
+
+    target_db = pymongo.MongoClient(
+        "mongodb://127.0.0.1:3001")
+
+    db = target_db.meteor
+    pos = etl.fromdicts(db.inventory.purchase_orders.find())
+
+    print(etl.header(pos))
+
     try:
-        result = load_data(db, table_name)
+        result = load_data(source_db, table_name)
         if result:
             # handle menu items first
             # @TODO - convert this to be reusable method
-            transform_menu_items(db, result)
+            transform_menu_items(result)
     finally:
-        db.close()
+        source_db.close()
 
 
-def load_data(db, table_name):
-    """
-    Manipulate the data extracted by the previous extract method
-    """
-    return etl.fromdb(db, "SELECT * from {0}".format(table_name))
-
-
-def transform_menu_items(db, data):
+def transform_menu_items(data):
     """
     Load the transformed data into the destination(s)
     +-------------+--------------+------+------+------+
@@ -72,17 +74,19 @@ def transform_menu_items(db, data):
     lab_results = etl.cut(menu_items, *range(11, 16))
 
     etl.tojson(menu_items, 'dump.json', sort_keys=True)
-
-    print(open('dump.json').read())
+    #print(source_count(menu_items))
+    #print(open('dump.json').read())
     
     #print(etl.look(lab_results))
 
 
-def source_count(self):
+def source_count(data):
     """
     Count the number of records from source(s)
     """
-    pass
+    if data is not None:
+        return etl.nrows(data)
+    return None
 
 
 def destination_count(self):
@@ -91,11 +95,23 @@ def destination_count(self):
     """
     pass
 
+def load_mongo_data(data):
+    return etl.fromdb()
+
+
+def load_data(db, table_name):
+    """
+    Manipulate the data extracted by the previous extract method
+    """
+    return etl.fromdb(db, "SELECT * from {0}".format(table_name))
+
+
 def dbview_to_list(data):
     if type(data) is DbView:
         # convert the db view to a lists of lists for petl
         # map is quicker than list comp
         return list(map(list, data))
+
 
 if __name__ == '__main__':
     extract(sys.argv[1])
