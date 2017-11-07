a344737d1eb3b9b8e083a737a7d86a52903319c9
Trevor Delamorandiere <tdelam@gmail.com>
2017-11-07 17:33:14 -0500

    Merged data to get ready for load, extracting lab results, moved code to methods

--- menu_items.py
+++ menu_items.py
@@ -20,6 +20,9 @@ from pattern.text.en import singularize
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+# sanitize categories, need a better way to do this, perhaps a stemming lib
+PLURAL_CATEGORIES = ['Seeds', 'Drinks', 'Edibles']
+
 
 def extract(table_name, collection):
     """
@@ -48,18 +51,10 @@ def extract(table_name, collection):
 
 def transform_menu_items(source_data, target_data, source_ctx, target_ctx):
     """
-    Load the transformed data into the destination(s)
-    +-------------+--------------+------+------+------+
-    | thc_percent | thca_percent | cbn  | cbd  | cbda |
-    +=============+==============+======+======+======+
-    |         0.0 |          0.0 |  0.0 |  0.0 |  0.0 |
-    +-------------+--------------+------+------+------+
-    |         0.0 |          0.0 |  0.0 |  0.0 |  0.0 |
-    +-------------+--------------+------+------+------+
+    Transform data
     """
     # source data table
     source_dt = view_to_list(source_data)
-    #print("{0}".format(etl.header(source_data)))
     # Cut out all the fields we don't need to load
     menu_items = etl.cutout(source_dt, 'menu_id', 'body_html',
                             'deduct_from_id', 'tested_by', 'medicine_amount',
@@ -94,23 +89,32 @@ def transform_menu_items(source_data, target_data, source_ctx, target_ctx):
     # Then we nede a dict of categories to compare against.
     # id is stored to match against when transforming and mapping categories
     source_ctx = dict([(value, id) for (value, id) in source_values])
-    target_category_names = [name for name in target_ctx.distinct('name')]
-
     # find the menu item category id
-    menu_items_cat_id = etl.values(menu_items, 'category_id')
+    mmj_ids = etl.values(menu_items, 'category_id')
 
-    # sanitize categories, need a better way to do this, perhaps a stemming lib
-    plural_categories = ['Seeds', 'Drinks', 'Edibles']
+    cat_prods = category_products(menu_items, mmj_ids, source_ctx, target_ctx)
+    cat_genetics = genetics_products(menu_items, target_ctx)
+    cat_names = category_names(menu_items, target_ctx)
+    strains = strain_names(menu_items, target_ctx)
 
+    #print(strains)
+    # merge all 3 tables into one... git'r ready for import!
+    merged_data = etl.merge(cat_prods, cat_genetics, cat_names, key='id')
+
+    #print(merged_data.lookall())
+
+
+def category_products(menu_items, mmj_cat_ids, source_ctx, target_ctx):
     # dict to hold category transformations
-    transformed_categories = {}
+    target_category_names = [name for name in target_ctx.distinct('name')]
+    categories_dict = {}
     # little scheme to match cats for transforming proper categories
-    for item in menu_items_cat_id:
+    for item in mmj_cat_ids:
         # Separates the dictionary's values in a list, finds the position of
         # the value and gets the key at that position to match the id
         # returns source category name to compare
         source_cat_name = source_ctx.keys()[source_ctx.values().index(item)]
-        if source_cat_name in plural_categories:
+        if source_cat_name in PLURAL_CATEGORIES:
             source_cat_name = singularize(source_cat_name)
         # first condition, if the mmj category is found in g1
         # category, we'll assign it.
@@ -120,39 +124,47 @@ def transform_menu_items(source_data, target_data, source_ctx, target_ctx):
             target_cat_id = target_ctx.collection.find_one(
                 {'name': source_cat_name})
 
-            transformed_categories[item] = target_cat_id.get('_id')
-    
+            categories_dict[item] = target_cat_id.get('_id')
+
     category_map = OrderedDict()
+    category_map["id"] = "id"
+    category_map["name"] = "name"
     category_map["category_id"] = "category_id", \
-        lambda value: transformed_categories[value]
+        lambda value: categories_dict[value]
+    cat = etl.fieldmap(menu_items, category_map)
+    return cat
 
-    categories_cat_ids = etl.fieldmap(menu_items, category_map)
-    #print(table1.lookall())
 
-    # @TODO - put this into a method for cleanliness
+def genetics_products(menu_items, target_ctx):
     # if mmj product genetics field contains any of the g1 category names.
-    transformed_genetics = {}
+    target_category_names = [name for name in target_ctx.distinct('name')]
+    genetics_dict = {}
     genetics = etl.facet(menu_items, 'genetics')
     for genetic in genetics.keys():
         if genetic and genetic in target_category_names:
             target_cat_id = target_ctx.collection.find_one(
                 {'name': genetic})
-            split_rows = etl.select(menu_items, 'genetics', 
-                lambda value: value == genetic)
+            split_rows = etl.select(
+                menu_items, 'genetics', lambda value: value == genetic
+            )
             filtered_genetics = split_rows.values('genetics')
             for filtered_genetic in filtered_genetics:
-                transformed_genetics[filtered_genetic] = target_cat_id.get('_id')
+                genetics_dict[filtered_genetic] = target_cat_id.get('_id')
 
     genetics_map = OrderedDict()
+    genetics_map["id"] = "id"
+    genetics_map["name"] = "name"
     genetics_map["category_id"] = "genetics", \
-        lambda value: transformed_genetics[value]
+        lambda value: genetics_dict[value]
 
-    genetics_cat_ids = etl.fieldmap(menu_items, genetics_map)
+    genetics_cats = etl.fieldmap(menu_items, genetics_map)
+    return genetics_cats
 
-    # cat = etl.cat(table1, table2)
-    # print(cat.lookall())
 
+def category_names(menu_items, target_ctx):
+    target_category_names = [name for name in target_ctx.distinct('name')]
     # if mmj product name contains any of the g1 category names
+    products_dict = {}
     product_names = etl.facet(menu_items, 'name')
     for name in product_names:
         if name:
@@ -160,29 +172,45 @@ def transform_menu_items(source_data, target_data, source_ctx, target_ctx):
             for cat in split_names:
                 if cat in target_category_names:
                     target_cat_id = target_ctx.collection.find_one(
-                        {'name': name})
+                        {'name': cat})
+                    products_dict[name] = target_cat_id.get('_id')
 
-                    print("name - %s" % target_cat_id)
+    products_map = OrderedDict()
+    products_map["id"] = "id"
+    products_map["name"] = "name"
+    products_map["category_id"] = "name", \
+        lambda value: products_dict[value]
 
-    # third condition, calculate sativa/indica percentage, if either has more
+    products_cats = etl.fieldmap(menu_items, products_map)
+    return products_cats
+
+
+def strain_names(menu_items, target_ctx):
+    # calculate sativa/indica percentage, if either has more
     # than 80% choose that type as the category, otherwise make it hybrid
+    target_cat_id = {}
     strain = etl.cut(menu_items, 'sativa', 'indica')
     strain_vals = etl.dicts(view_to_list(strain))
-
     for strain in strain_vals.__iter__():
         if strain['sativa'] > 0 or strain['indica'] > 0:
             if strain['sativa'] >= 80:
                 # sativa
-                pass
+                category = target_ctx.collection.find_one({'name': 'Sativa'})
+                print(category.get('_id'))
+                target_cat_id[strain['sativa']] = category.get('_id')
             elif strain['indica'] >= 80:
                 # indica
-                pass
+                target_cat_id = target_ctx.collection.find_one({'name': 'Indica'})
             else:
                 # hybrid
-                pass
+                target_cat_id = target_ctx.collection.find_one({'name': 'Hybrid'})
+    print(target_cat_id)
 
+def lab_results(data):
     # Put lab results on their own as this will be its own collection later
-    lab_results = etl.cut(menu_items, *range(11, 16))
+    lab_results = etl.cut(data, *range(11, 16))
+    return lab_results
+
 
 def source_count(data):
     """
