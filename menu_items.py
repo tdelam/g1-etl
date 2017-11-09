4a1ac119f4e5bc1f9e19fbe05f5e6ed1149b2639
Trevor Delamorandiere <tdelam@gmail.com>
2017-11-09 09:44:30 -0500

    importing products into G1 now. Need to cleanup some things and add more fields

--- menu_items.py
+++ menu_items.py
@@ -2,17 +2,17 @@ from __future__ import division, print_function, absolute_import
 
 import MySQLdb
 import sys
-import itertools
 import pymongo
-import re
 import petl as etl
+import uuid
+import json
+import logging
+
 
 from petl.io.db import DbView
 from petl.io.json import DictsView
 from petl.transform.basics import CutView
 from collections import OrderedDict
-from pymongo import MongoClient
-from sqlalchemy import *
 
 from pattern.text.en import singularize
 
@@ -22,6 +22,9 @@ sys.setdefaultencoding('utf8')
 
 # sanitize categories, need a better way to do this, perhaps a stemming lib
 PLURAL_CATEGORIES = ['Seeds', 'Drinks', 'Edibles']
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def extract(table_name, collection):
@@ -39,17 +42,17 @@ def extract(table_name, collection):
         source_data = load_db_data(source_db, table_name)
         source_ctx = load_db_data(source_db, 'categories')
 
-        target_data = load_mongo_data(target_db, '{0}'.format(collection))
+        target_data = load_mongo_data(target_db, 'inventory.products')
         target_ctx = load_mongo_data(target_db, 'inventory.categories')
 
-        transform_menu_items(source_data, target_data, source_ctx, target_ctx)
+        transform_menu_items(source_data, source_ctx, target_ctx, target_data)
 
     finally:
         source_db.close()
         target_db.close()
 
 
-def transform_menu_items(source_data, target_data, source_ctx, target_ctx):
+def transform_menu_items(source_data, source_ctx, target_ctx, target_data):
     """
     Transform data
     """
@@ -59,7 +62,7 @@ def transform_menu_items(source_data, target_data, source_ctx, target_ctx):
     menu_items = etl.cutout(source_dt, 'menu_id', 'body_html',
                             'deduct_from_id', 'tested_by', 'medicine_amount',
                             'medicine_measurement', 'batch_number', 'barcode',
-                            'custom_barcode', 'taxable', 'image_updated_at',
+                            'custom_barcode', 'image_updated_at',
                             'consignment', 'mmjrevu_id', 'sclabs_report_id',
                             'override_global_dollars_to_points',
                             'dollars_to_points', 'ingredients', 'created_at',
@@ -68,23 +71,10 @@ def transform_menu_items(source_data, target_data, source_ctx, target_ctx):
                             'olcc_medical_grade', 'thc_percent_min',
                             'thca_percent_min')
 
-    # Rename source fields to match target fields, rename id to mmjmenuid so
-    # we can track related data
-    new_header = {
-        'id': 'mmjmenuid',
-        'vendor_id': 'vendorId',
-        'category_id': 'categoryId',
-        'body': 'description'
-    }
-
-    renamed_header = etl.rename(menu_items, new_header)
-
     # Two-step transform and cut. First we need to cut the name
     # and id from the source data to map to.
     cut_source_cats = etl.cut(source_ctx, 'name', 'id')
     source_values = etl.values(cut_source_cats, 'name', 'id')
-    source_product_names = etl.values(menu_items, 'name')
-    source_desc = etl.values(menu_items, 'body')
 
     # Then we nede a dict of categories to compare against.
     # id is stored to match against when transforming and mapping categories
@@ -95,13 +85,73 @@ def transform_menu_items(source_data, target_data, source_ctx, target_ctx):
     cat_prods = category_products(menu_items, mmj_ids, source_ctx, target_ctx)
     cat_genetics = genetics_products(menu_items, target_ctx)
     cat_names = category_names(menu_items, target_ctx)
-    strains = strain_names(menu_items, target_ctx)
 
-    #print(strains)
     # merge all 3 tables into one... git'r ready for import!
     merged_data = etl.merge(cat_prods, cat_genetics, cat_names, key='id')
+    print(merged_data.lookall())
+    other_category = {
+        "_id": random_mongo_id(),
+        "organizationId": "420",
+        "name": "Other",
+        "tax_free": "false",
+        "fixtureImage": "/northernlights/images/growone-logo-pink.svg",
+    }
 
-    #print(merged_data.lookall())
+    print(menu_items.lookall())
+
+    # create "other" category, fill in None with Other and
+    # merge it in with the other data table
+    etl.values(merged_data, 'category_id')
+    insert_id = None
+    category_exists = target_ctx.collection.find_one({"name": "Other"})
+    if category_exists:
+        insert_id = category_exists["_id"]
+    else:
+        insert_id = target_ctx.collection.insert(other_category)
+    data_dict = {}
+    for data in etl.values(merged_data, 'category_id'):
+        if data is None:
+            data_dict[data] = insert_id
+
+    # product_dict = {
+    #         name: product.name,
+    #     shareOnWM: false,
+    #     restockLevel: 0,
+    #     description: this.stripTags(product.body_html),
+    #     price: Number(price),
+    #     unitOfMeasure: UnitOfMeasure[uom],
+    #     netMarijuana: 0,
+    #     categoryId: product.categoryId, // Move them to uncategorized for now so they can be moved.
+    #     _categoryName: product.category,
+    #     publicImageUrl: image,
+    #     pricingTier: uom === 'GRAM' ? pricingTier : null,
+    #     weightPricing: weightPricing,
+    #     active: !product.on_hold,
+    #     stockQuantity: 0
+    # }
+
+    mapping = OrderedDict()
+    mapping["_id"] = 'random_mongo_id()'
+    mapping["id"] = "id"
+    mapping["name"] = "name"
+    mapping["organizationId"] = "420"
+    mapping["category_id"] = "category_id", lambda x: data_dict[x]
+
+    merged_data = etl.merge(merged_data,
+                            etl.fieldmap(merged_data, mapping), key='id')
+
+    print(merged_data.lookall())
+
+    etl.tojson(merged_data, 'example.json', sort_keys=True)
+
+    json_items = open("example.json")
+
+    parsed = json.loads(json_items.read())
+
+    for item in parsed:
+        item['_id'] = random_mongo_id()
+        print(item)
+        target_data.collection.insert(item)
 
 
 def category_products(menu_items, mmj_cat_ids, source_ctx, target_ctx):
@@ -129,9 +179,11 @@ def category_products(menu_items, mmj_cat_ids, source_ctx, target_ctx):
     category_map = OrderedDict()
     category_map["id"] = "id"
     category_map["name"] = "name"
+    category_map["organizationId"] = "420"
     category_map["category_id"] = "category_id", \
         lambda value: categories_dict[value]
     cat = etl.fieldmap(menu_items, category_map)
+    logger.info('category_id %s', cat)
     return cat
 
 
@@ -154,6 +206,7 @@ def genetics_products(menu_items, target_ctx):
     genetics_map = OrderedDict()
     genetics_map["id"] = "id"
     genetics_map["name"] = "name"
+    genetics_map["organizationId"] = "420"
     genetics_map["category_id"] = "genetics", \
         lambda value: genetics_dict[value]
 
@@ -178,33 +231,33 @@ def category_names(menu_items, target_ctx):
     products_map = OrderedDict()
     products_map["id"] = "id"
     products_map["name"] = "name"
+    products_map["organizationId"] = "420"
     products_map["category_id"] = "name", \
         lambda value: products_dict[value]
 
     products_cats = etl.fieldmap(menu_items, products_map)
+    # print(products_dict)
     return products_cats
 
 
 def strain_names(menu_items, target_ctx):
     # calculate sativa/indica percentage, if either has more
     # than 80% choose that type as the category, otherwise make it hybrid
-    target_cat_id = {}
-    strain = etl.cut(menu_items, 'sativa', 'indica')
-    strain_vals = etl.dicts(view_to_list(strain))
+    target_cat = {}
+    strain_data = etl.cut(menu_items, 'id', 'name', 'sativa', 'indica')
+    strain_vals = etl.dicts(view_to_list(strain_data))
     for strain in strain_vals.__iter__():
         if strain['sativa'] > 0 or strain['indica'] > 0:
             if strain['sativa'] >= 80:
                 # sativa
-                category = target_ctx.collection.find_one({'name': 'Sativa'})
-                print(category.get('_id'))
-                target_cat_id[strain['sativa']] = category.get('_id')
+                pass
             elif strain['indica'] >= 80:
                 # indica
-                target_cat_id = target_ctx.collection.find_one({'name': 'Indica'})
+                pass
             else:
                 # hybrid
-                target_cat_id = target_ctx.collection.find_one({'name': 'Hybrid'})
-    print(target_cat_id)
+                pass
+
 
 def lab_results(data):
     # Put lab results on their own as this will be its own collection later
@@ -252,6 +305,16 @@ def view_to_list(data):
 
     if type(data) is DictsView:
         return data
+
+
+def random_mongo_id():
+    """
+    Returns a random string of length 17
+    """
+    random = str(uuid.uuid4())
+    random = random.replace("-", "")
+
+    return random[0:17]
 
 
 if __name__ == '__main__':
