ce0102b5a4d64689b54ab35abbeff75dee604352
Trevor Delamorandiere <tdelam@gmail.com>
2017-11-27 14:34:24 -0500

    test code for rest api

--- jwt-test.py
+++ jwt-test.py
@@ -1,13 +1,41 @@
 import jwt
+import requests
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from cryptography.hazmat.primitives.serialization import load_pem_public_key
 
+SHARED_KEY = {
+    'key': '8rDLYiMzi5GqtS8Ntu7kH21bWYrHAe54'
+}
+
+def post_vendors(token):
+    url = 'http://localhost:3004/api/mmjetl/load/vendors'
+    headers = {'Authorization': 'Bearer {0}'.format(token)}
+    vendor = {
+        'website': '',
+        'id': 29368245,
+        'entityType': 'vendor',
+        'licenceNumber': '84298845BD',
+        'phone': '8165546978',
+        'address': {
+            'city': 'Modesto',
+            'zip': '51218',
+            'country': '',
+            'line2': '',
+            'line1': '89742 E. Shore Ave',
+            'state': 'CA'
+        },
+        'name': 'A Green Thmb',
+        'accountStatus': 'ACTIVE',
+    }
+    r = requests.post(url, data=vendor, headers=headers);
+    return r
+
 
 def encode():
-    return jwt.encode({'some': 'payload'},
-                      load_private_key(), algorithm='RS256')
+    encode = jwt.encode(SHARED_KEY, load_private_key(), algorithm='RS256')
+    return encode
 
 
 def decode():
@@ -30,6 +58,6 @@ def load_public_key():
 
 
 if __name__ == '__main__':
-    encode()
-    decode()
-    import pdb; pdb.set_trace()
+    # print(encode())
+    # print(decode())
+    post_vendors(encode())
