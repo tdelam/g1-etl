6d812c6dc8afb68914f2edcd63eda8ee1c8513ab
Trevor Delamorandiere <tdelam@gmail.com>
2017-12-05 23:47:47 -0500

    employees complete, may need tweaking to make sure its inline with the model

---
+++ employees.py
@@ -0,0 +1,131 @@
+from __future__ import division, print_function, absolute_import
+
+import sys
+import MySQLdb
+import petl as etl
+import json
+import logging
+import logging.handlers
+
+from petl.io.db import DbView
+from petl.io.json import DictsView
+from petl.transform.basics import CutView
+from collections import OrderedDict
+from utilities import utils, g1_jwt
+
+
+logging.basicConfig(filename="logs/g1-etl-employees.log", level=logging.INFO)
+log = logging.getLogger("g1-etl-employees")
+
+# handle characters outside of ascii
+reload(sys)
+sys.setdefaultencoding('latin-1')
+
+
+def extract(organization_id):
+    """
+    Grab all data from source(s).
+    """
+    source_db = MySQLdb.connect(host="localhost",
+                                user="root",
+                                passwd="c0l3m4N",
+                                db="mmjmenu_development")
+
+    try:
+        mmj_employees = load_db_data(source_db, 'users')
+        mmj_dispensary_users = load_db_data(source_db, 'dispensary_users')
+
+        transform(mmj_employees, mmj_dispensary_users, organization_id)
+
+    finally:
+        source_db.close()
+
+
+def transform(mmj_employees, mmj_dispensary_users, organization_id):
+    """
+    Load the transformed data into the destination(s)
+    """
+    # source data table
+    source_dt = view_to_list(mmj_employees)
+    roles_dt = view_to_list(mmj_dispensary_users)
+
+    cut_data = ['id', 'email', 'first_name', 
+                'last_name', 'created_at', 'updated_at']
+    
+    cut_roles = ['id', 'access']
+
+    employee_data = etl.cut(source_dt, cut_data)
+    roles_data = etl.cut(roles_dt, cut_roles)
+    #print(type(roles_data))
+    #import pdb; pdb.set_trace()
+    employees = (
+        etl
+        .addfield(employee_data, 'organizationId')
+        .addfield('mmKeys')
+        .addfield('role')
+    )
+
+    lookup_role = etl.lookup(roles_data, 'id', 'access')
+
+    mappings = OrderedDict()
+    mappings['id'] = 'id'
+    mappings['email'] = 'email'
+    mappings['first_name'] = 'first_name'
+    mappings['last_name'] = 'last_name'
+    mappings['role'] = lambda x: lookup_role[x.id][0]
+    mappings['createdAt'] = 'created_at'
+    mappings['updatedAt'] = 'updated_at'
+    mappings['organizationId'] = organization_id
+    mappings['accountStatus'] = \
+        lambda x: "ACTIVE" if x.confirmed == 1 else "INACTIVE"
+
+    fields = etl.fieldmap(employees, mappings)
+    merged_employees = etl.merge(employees, fields, key='id')
+    
+    mapped_employees = []
+    for item in etl.dicts(merged_employees):
+        item['keys'] = {
+            'id': item['id']
+        }
+        # set up final structure for API
+        mapped_employees.append(item)
+
+    print(mapped_employees)
+
+def source_count(mmj_employees):
+    """
+    Count the number of records from source(s)
+    """
+    if mmj_employees is not None:
+        return etl.nrows(mmj_employees)
+    return None
+
+
+def destination_count(dest_data):
+    """
+    Same as source_count but with destination(s)
+    """
+    if dest_data is not None:
+        return etl.nrows(dest_data)
+    return None
+
+
+def load_db_data(db, table_name):
+    """
+    Data extracted from source db
+    """
+    return etl.fromdb(db, "SELECT * from {0} LIMIT 10".format(table_name))
+
+
+def view_to_list(data):
+    if type(data) is DbView or type(data) is CutView:
+        # convert the view to a lists of lists for petl
+        # map is quicker than list comp
+        return list(map(list, data))
+
+    if type(data) is DictsView:
+        return data
+
+
+if __name__ == '__main__':
+    extract(sys.argv[1])
