0d5dfd40e54db486da2ed20987bfb6f616efb051
Chad Armstrong <chad@armstrong.tech>
2017-12-06 14:52:48 -0500

    Add physicians extract, remove json_serial function and import it from utils

--- etl_import.py
+++ etl_import.py
@@ -7,18 +7,12 @@ import menu_items
 import vendors
 import physicians
 
-def json_serial(obj):
-    """JSON serializer for objects not serializable by default json code"""
-    if isinstance(obj, (datetime, date)):
-        return obj.isoformat()
-    raise TypeError("Type %s not serializable" % type(obj))
-
 def extract(organization_id):
     employees_extract = employees.extract(organization_id)
     members_extract = members.extract(organization_id)
     menu_items_extract = menu_items.extract(organization_id)
     vendors_extract = vendors.extract(organization_id)
-    employees_extract = employees.extract(organization_id)
+    physicians_extract = physicians.extract(organization_id)
 
     # do some magic to pickle all of the extracts into
     # a single json structure that can be persisted into
@@ -28,9 +22,9 @@ def extract(organization_id):
         'members': members_extract,
         'products': menu_items_extract,
         'vendors': vendors_extract,
-        'employees': employees_extract
+        'physicians': physicians_extract
     }
-    result = json.dumps(payload, sort_keys=True, indent=4, default=json_serial)
+    result = json.dumps(payload, sort_keys=True, indent=4, default=utils.json_serial)
     print(result)
 
 
