73ae17749f51f085c7da10aeed8434386d516afb
Trevor Delamorandiere <tdelam@gmail.com>
2017-12-08 13:28:51 -0500

    moved utilities over

---
+++ entities/utilities/utils.py
@@ -0,0 +1,95 @@
+import urllib2
+import logging
+import logging.handlers
+from datetime import date, datetime
+
+from pymongo import MongoClient
+
+import petl as etl
+from petl.io.db import DbView
+from petl.io.json import DictsView
+from petl.transform.basics import CutView
+from datetime import date, datetime
+
+logging.basicConfig(filename="g1-etl-utils.log", level=logging.INFO)
+log = logging.getLogger("g1-etl-utils")
+
+
+def load_db_data(db, table_name):
+    """
+    Data extracted from source db
+    """
+    return etl.fromdb(db, "SELECT * from {0} LIMIT 10".format(table_name))
+
+
+def view_to_list(data):
+    if type(data) is DbView or type(data) is CutView:
+        # convert the view to a lists of lists for petl
+        # map is quicker than list comp
+        return list(map(list, data))
+
+    if type(data) is DictsView:
+        return data
+
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError("Type %s not serializable" % type(obj))
+
+
+def download_images(env, user_id, pic):
+    """
+    Downloading MMJ with progress logging
+    """
+    remote = (r"https://wm-mmjmenu-images-{0}.s3.amazonaws.com/"
+              "customers/pictures/{1}/large/{2}").format(env, user_id, pic)
+    local = str(user_id) + ".jpg"
+
+    url = urllib2.urlopen(remote)
+    header = url.info()
+    total_size = int(header["Content-Length"])
+
+    log.info("---"
+             "Downloading {0} bytes of image {1}...".format(total_size, pic))
+    fp = open(local, 'wb')
+
+    block_size = 8192
+    count = 0
+    while True:
+        chunk = url.read(block_size)
+        if not chunk:
+            break
+        fp.write(chunk)
+        count += 1
+        if total_size > 0:
+            percent = int(count * block_size * 100 / total_size)
+            if percent > 100:
+                percent = 100
+            log.info("%2d%%" % percent)
+            if percent < 100:
+                log.info("\b\b\b\b\b")  # Erase "NN% "
+            else:
+                log.info("---"
+                         "{0} successfully downloaded.".format(pic))
+
+    fp.flush()
+    fp.close()
+    if not total_size:
+        log.error("--"
+                  "Error: File {0} did not successfully download".format(pic))
+
+
+def chunks(data, size):
+    """
+    chunks big data so we can send the API data chunks
+    """
+    chunk = [data[i:i + size] for i in range(0, len(data), size)]
+    return chunk
+
+
+def mongo_connect_and_insert(payload):
+    db = MongoClient('mongodb://localhost:3005/')
+    imports = db.meteor.etl.imports
+    imports.insert_one(payload)
