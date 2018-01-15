ebbe2e29f733a96a364980081a5038b08f377f11
Trevor Delamorandiere <tdelam@gmail.com>
2018-01-15 13:24:02 -0500

    db host has changed

--- mmj/entities/menu_items.py
+++ mmj/entities/menu_items.py
@@ -34,9 +34,8 @@ def extract(dispensary_id, organization_id, debug):
     """
     Grab all data from source(s).
     """
-    source_db = MySQLdb.connect(host="mmjmenu-production-copy-playground-10171"
-                                "7-cluster.cluster-cmtxwpwvylo7.us-west-2.rds"
-                                ".amazonaws.com",
+    source_db = MySQLdb.connect(host="mmjmenu-production-copy-playground-011218"
+                                     ".cmtxwpwvylo7.us-west-2.rds.amazonaws.com",
                                 user="mmjmenu_app",
                                 passwd="V@e67dYBqcH^U7qVwqPS",
                                 db="mmjmenu_production")
